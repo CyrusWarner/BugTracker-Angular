aff1f4bcbf20c6009e1703ebe0c0790c0c214846
CyrusWarner <cyruswarner27@gmail.com>
2021-12-20 23:04:08 -0600

    Added method that builds the registered user request object.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { UserService } from './shared/services/user-service.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [UserService],
 
--- src/app/shared/services/user-service.service.ts
+++ src/app/shared/services/user-service.service.ts
@@ -10,11 +10,23 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) {}
 
-   registerUser(userToRegister: UserRegister): Observable<any> {
-     const url = "localhost:4000/api/user/register"
-     return this.httpClient.post(url, userToRegister)
+   registerUser(registerUserForm: FormGroup): Observable<any> {
+     let user = this.buildRegisterUserRequest(registerUserForm)
+     const url = "http://localhost:4200/api/user/register"
+     return this.httpClient.post(url, user)
       .pipe(catchError(err => {
         return of(err)
       }))
+  }
+
+  buildRegisterUserRequest(registerUserForm: FormGroup) {
+    let user: UserRegister = {
+      firstName: registerUserForm.controls['firstName'].value,
+      lastName: registerUserForm.controls['lastName'].value,
+      email: registerUserForm.controls['email'].value,
+      password: registerUserForm.controls['password'].value
+    }
+
+    return user
   }
 }
