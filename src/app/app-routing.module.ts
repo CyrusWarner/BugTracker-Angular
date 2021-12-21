1d1b3188a071549cf4edc6b477ac6d22d618f237
CyrusWarner <cyruswarner27@gmail.com>
2021-12-20 18:37:53 -0600

    Added register component with form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
-import { UserService } from './shared/services/user-service.service';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Lazy loading the registermodule
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'register',
+    loadChildren: () => import('./components/register/register.module').then((m) => m.RegisterModule)
+  },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  providers: [UserService],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserService } from './shared/services/user-service.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
