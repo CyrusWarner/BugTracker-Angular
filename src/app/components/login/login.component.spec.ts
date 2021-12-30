a6eba3e546037c6fbeca559a6b435c37ec5eb9a0
CyrusWarner <cyruswarner27@gmail.com>
2021-12-29 19:04:15 -0600

    Added unit tests to the LoginComponent.

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../shared/services/user-service.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
@@ -5,10 +6,14 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let userServiceSpy:any
   beforeEach(async () => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['loginUser'])
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        {provide: UserService, useValue: userServiceSpy}
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +27,25 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('loginUser', () => {
+    it('should call the UserService loginUser function', () => {
+      const testLogin = {
+        email: "Testemail@gmail.com",
+        password: "FakePass"
+      }
+
+      component.loginUserForm.setValue(testLogin)
+      component.loginUser(component.loginUserForm)
+
+      expect(userServiceSpy.loginUser).toHaveBeenCalled()
+    })
+    it('should not call the UserService loginUser function', () => {
+      const testEmail = "Testemail@gmail.com" // Arrange
+
+      component.loginUserForm.get('email')?.setValue(testEmail) // Act
+      component.loginUser(component.loginUserForm)
+
+      expect(userServiceSpy.loginUser).not.toHaveBeenCalled() // Assert
+    })
+  })
 });
